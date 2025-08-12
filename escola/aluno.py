353e67310815448297ea532a639d2810fc809b0a
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-12 10:35:46 -0300

    função nova

--- escola/aluno.py
+++ escola/aluno.py
@@ -5,12 +5,21 @@ def calcular_media(notas:list[float]) ->float:
     # validando se a entrada é uma lista 
     if not isinstance (notas,list):
         raise TypeError("nota invalida")
+    
+    
     # validando lista vazia
     if len(notas) == 0:
         raise ValueError("não é permitido o valor ")
+    for i in notas:
+        if not isinstance(f,len) and not isinstance(f,float):
+            raise TypeError ('nota invalida')
+        elif  i > 0 and i < 0 :
+            res += 10
     media = sum(notas)/len(notas)
     return round(media,1)
-    
+
 
     
+
+
 
--- tests/test_calcular_media_enviando_string_e_numero_juntos.py
+++ tests/test_calcular_media_enviando_string_e_numero_juntos.py
@@ -5,5 +5,28 @@ def test_calcular_media_enviando_string_e_numero_juntos():
 # definindo entrada
     entrada=['ola',3.0]
 # executando  a função esperando erro
-    with pytest.raises(ValueError,match=" não é permitido numero e texto "):
+    with pytest.raises(TypeError,match=" não é permitido numero e texto "):
         calcular_media(entrada)
+
+
+    
+def test_calcular_media_com_numero_negativo():
+    entrada=['0,-10']
+# executando  a função esperando erro
+    with pytest.raises(TypeError,match=" não é permitido numeo negativo "):
+        calcular_media(entrada)
+    
+
+
+
+
+
+def test_calcular_media_com_numero_maior_que_10():
+    entrada=[8.0,10,11]
+    # executando  a função esperando erro
+    with pytest.raises(TypeError,match=" limite de nota\[0,10\]"):
+        calcular_media(entrada)
+
+
+
+
