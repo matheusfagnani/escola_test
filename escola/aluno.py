5d74070fca53f768324f3d421aac5e261af2b970
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-12 08:57:48 -0300

    função para verificar se a entrada é uma lista

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,8 +1,15 @@
 def calcular_media(notas:list[float]) ->float:
 
+
+
+    # validando se a entrada é uma lista 
+    if not isinstance (notas,list):
+        raise TypeError("nota invalida")
+    # validando lista vazia
     if len(notas) == 0:
         raise ValueError("não é permitido o valor ")
-    media = sum(notas)
+    media = sum(notas)/len(notas)
+    return round(media,1)
     
 
 
