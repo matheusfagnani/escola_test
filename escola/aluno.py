6e13a65e1f3aa52ad17a51c1dbee5b58968e821f
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-12 08:43:11 -0300

    test_de_softer

---
+++ escola/aluno.py
@@ -0,0 +1,9 @@
+def calcular_media(notas:list[float]) ->float:
+
+    if len(notas) == 0:
+        raise ValueError("não é permitido o valor ")
+    media = sum(notas)
+    
+
+
+
---
+++ tests/test_calcular_media_enviando_string.py
@@ -0,0 +1,9 @@
+import pytest
+from escola.aluno import calcular_media 
+
+def test_calcular_media_enviando_string():
+# definindo entrada
+    entrada= 'ola'
+# executando  a função esperando erro
+    with pytest.raises(ValueError,match="nota invalida "):
+        calcular_media(entrada)
