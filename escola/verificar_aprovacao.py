e73aa9c124d0047a919f786fa40d15cd160a2d2c
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-19 08:02:15 -0300

    funçao verifica aprovação

--- escola/verificar_aprovacao.py
+++ escola/verificar_aprovacao.py
@@ -1,7 +1,9 @@
-def verificar_aprovacao(media :[float]) -> str: 
-   if nota <0:
-      return 
-        
-    
-        
+def verificar_aprovacao(media: float) -> str:
+    if media < 0:
+        return "Média inválida"
+    if media >= 7.0:
+        return "Aprovado"
+    if media >= 5.0:
+        return "Recuperação"
+    return "Reprovado"
     
